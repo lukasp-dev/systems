0aa7fba7a898d283a5f4fec564da32c5178d475e
Jewook Park <nubddak3@gmail.com>
2026-07-09 14:56:31 -0700

    updated

--- data_structures/skipList/skipList.cpp
+++ data_structures/skipList/skipList.cpp
@@ -6,51 +6,45 @@
  * are expected O(log n). Space is expected O(n)
  */
 
-#include <iostream>
-#include <vector>
-#include <random>
-#include <climits>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-class SkipList {
+class SkipList{
 private:
     struct Node {
         int val;
         vector<Node*> next;
-        
-        Node(int v, int level) : val(v), next(level, nullptr) {}
+
+        Node(int v, int level):val(v), next(level, nullptr){}
     };
 
-    static constexpr int MAX_LEVEL = 32;
-    Node* head;
-    int currentLevel;
-
-    int randomLevel(){
+    int getRandomHeight(){
         static random_device rd;
         static mt19937 gen(rd());
-        static uniform_int_distribution<int> dist(0, 1);
-
-        int lvl = 1;
-
-        while(lvl < MAX_LEVEL && dist(gen) == 1){
-            ++lvl;
-        }
+        static uniform_int_distribution dist(0, 1);
         
-        return lvl;
-    }
+        int randomLevel = 1;
+        while(randomLevel < MAX_LEVEL && dist(gen) == 1){
+            ++randomLevel;
+        }
 
+        return randomLevel;
+    }
+    
+    static int constexpr MAX_LEVEL = 32;
+    Node* head;
+    int currentLevel; // tracking the current height
 public:
-    SkipList() {
+    SkipList(){
         head = new Node(INT_MIN, MAX_LEVEL);
         currentLevel = 1;
     }
 
     bool search(int target){
         Node* cur = head;
-        
-        for(int level = currentLevel - 1; level >=0; --level){
-            while(cur->next[level] && cur->next[level]->val < target) {
+
+        for(int level = currentLevel - 1; level>=0; --level){
+            while(cur->next[level] && cur->next[level]->val < target){
                 cur = cur->next[level];
             }
         }
@@ -60,63 +54,63 @@ public:
         return cur && cur->val == target;
     }
 
-    void add(int num){
-        vector<Node*> update(MAX_LEVEL, nullptr);
+    void add(int v){
         Node* cur = head;
+        vector<Node*> update(MAX_LEVEL, nullptr);
 
-        for (int level = currentLevel - 1; level >= 0; --level){
-            while (cur->next[level] && cur->next[level]->val < num){
+        for(int level = currentLevel - 1; level>=0; --level){
+            while(cur->next[level] && cur->next[level]->val < v){
                 cur = cur->next[level];
             }
             update[level] = cur;
         }
 
-        int nodeLevel = randomLevel();
-
-        if (nodeLevel > currentLevel){
-            for (int level = currentLevel; level < nodeLevel; ++level){
+        int randomLevel = getRandomHeight();
+        if(randomLevel > currentLevel){
+            for(int level = currentLevel; level < randomLevel; ++level){
                 update[level] = head;
             }
-            currentLevel = nodeLevel;
+
+            currentLevel = randomLevel;
         }
-    
-        Node* newNode = new Node(num, nodeLevel);
-        
-        for (int level = 0; level < nodeLevel; ++level){
+
+        Node* newNode = new Node(v, randomLevel);
+
+        for(int level = 0; level < randomLevel; ++level){
             newNode->next[level] = update[level]->next[level];
             update[level]->next[level] = newNode;
         }
     }
 
-    bool erase(int num) {
-        vector<Node*> update(MAX_LEVEL, nullptr);
+    bool erase(int target){
         Node* cur = head;
-        
-        for (int level = currentLevel - 1; level >=0; --level) {
-            while (cur->next[level] && cur->next[level]->val < num){
-                cur = cur->next[level];
+        vector<Node*> update(MAX_LEVEL, nullptr);
+
+        for(int level = currentLevel - 1; level >= 0; --level){
+            while(cur->next[level] && cur->next[level]->val < target){
+                cur = cur->next[level]; 
             }
             update[level] = cur;
         }
         
-        Node * target = update[0]->next[0];
+        Node* erase_node = update[0]->next[0];
 
-        if (!target || target->val != num) return false;
+        if(!erase_node || erase_node->val != target) return false;
 
-        for(int level = 0; level < currentLevel; ++level) {
-            if (update[level]->next[level] != target) break;
+        for(int level = 0; level < currentLevel; ++level){
+            if(update[level]->next[level] != erase_node) continue;
+            update[level]->next[level] = erase_node->next[level];
         }
-        update[level]->next[level] = target->next[level];
+
+        delete erase_node;
+
+        while(currentLevel > 1 && head->next[currentLevel - 1] == nullptr) --currentLevel;
+
+        return true;
     }
-
-    delete target;
-
-    while(currentLevel > 1 && !head->next[currentLevel - 1]) --currentLevel;
-
-    return true;
 };
 
 int main(){
-
+    
     return 0;
 }
